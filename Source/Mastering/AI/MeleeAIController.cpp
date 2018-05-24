cbb90eb78df0e70addf52216b29eedb18c0eb28a
matt.edmonds <mattedmonds404@gmail.com>
2018-05-24 12:45:53 +0200

    Incremental check-in, moved hearing and target setting/access to C++ and simplified BP & BB sides.

--- Source/Mastering/AI/MeleeAIController.cpp
+++ Source/Mastering/AI/MeleeAIController.cpp
@@ -1,7 +1,44 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "MeleeAIController.h"
+#include "MasteringCharacter.h"
+#include "Components/SphereComponent.h"
 
 
 
+AMeleeAIController::AMeleeAIController(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	HearingSphere = CreateDefaultSubobject<USphereComponent>(TEXT("HearingSphere"));
+	HearingSphere->InitSphereRadius(HearingRadius);
+	HearingSphere->SetCollisionObjectType(ECC_Pawn);
+	HearingSphere->SetCollisionProfileName("OverlapOnlyPawn");
+
+	HearingSphere->OnComponentBeginOverlap.AddDynamic(this, &AMeleeAIController::OnHearingOverlap);
+
+	bAttachToPawn = true;
+}
+
+
+class AMasteringCharacter* AMeleeAIController::GetTarget()
+{
+	return CurrentTarget;
+}
+
+void AMeleeAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	HearingSphere->AttachTo(this->GetRootComponent(), NAME_None, EAttachLocation::SnapToTarget);
+}
+
+void AMeleeAIController::OnHearingOverlap(UPrimitiveComponent* OverlappedComp, AActor* Other, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	AMasteringCharacter *Target = Cast<AMasteringCharacter>(Other);
+	if (CurrentTarget != nullptr || CurrentTarget != Target)
+	{
+		CurrentTarget = Target;
+		OnTargetChange(CurrentTarget);
+	}
+}
 
--- Source/Mastering/AI/MeleeAIController.h
+++ Source/Mastering/AI/MeleeAIController.h
@@ -16,6 +16,35 @@ class MASTERING_API AMeleeAIController : public AAIController
 	
 public:
 	
+	AMeleeAIController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnTargetChange(class AMasteringCharacter* Target);
+
+	UFUNCTION(BlueprintCallable)
+	class AMasteringCharacter* GetTarget();
+
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Targeting")
+	class USphereComponent* HearingSphere;
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Targeting")
 	float HearingRadius = 1000.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Targeting")
+	float SightRadius = 2000.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Targeting")
+	float SightAngle = 60.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Targeting")
+	float BreakoffRadius = 1500.0f;
+
+protected:
+	UFUNCTION()
+	void OnHearingOverlap(UPrimitiveComponent* OverlappedComp, AActor* Other, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UPROPERTY()
+	AMasteringCharacter* CurrentTarget = nullptr;
 };
