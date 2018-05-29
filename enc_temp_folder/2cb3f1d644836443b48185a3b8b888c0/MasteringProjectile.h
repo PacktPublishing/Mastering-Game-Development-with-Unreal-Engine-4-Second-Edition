45bd6033618c18be33641ff02917195c465d3f83
matt.edmonds <mattedmonds404@gmail.com>
2018-05-29 21:58:19 +0200

    Merging chapter 5 to master.

---
+++ enc_temp_folder/2cb3f1d644836443b48185a3b8b888c0/MasteringProjectile.h
@@ -0,0 +1,40 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MasteringProjectile.generated.h"
+
+UCLASS(config=Game, transient)
+class AMasteringProjectile : public AActor
+{
+	GENERATED_BODY()
+
+	/** Sphere collision component */
+	UPROPERTY(VisibleDefaultsOnly, Category=Projectile)
+	class USphereComponent* CollisionComp;
+
+	/** Projectile movement component */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	class UProjectileMovementComponent* ProjectileMovement;
+
+public:
+	AMasteringProjectile();
+
+	/** called when projectile hits something */
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+	/** Returns CollisionComp subobject **/
+	FORCEINLINE class USphereComponent* GetCollisionComp() const { return CollisionComp; }
+	/** Returns ProjectileMovement subobject **/
+	FORCEINLINE class UProjectileMovementComponent* GetProjectileMovement() const { return ProjectileMovement; }
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Damage")
+	float DamageAmount = 5.0f;
+
+//	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Damage")
+//	class AController* Owner;
+};
+
