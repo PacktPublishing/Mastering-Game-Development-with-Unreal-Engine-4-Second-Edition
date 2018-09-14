21d26cb2cd3a5fc12c581b8264c4695c272c65f7
matt.edmonds <mattedmonds404@gmail.com>
2018-09-14 23:05:29 +0200

    Initial VR project submission.

---
+++ Build/Android/src/com/epicgames/ue4/DownloadShim.java
@@ -0,0 +1,33 @@
+package com.epicgames.ue4;
+
+import com.YourCompany.MasteringVR.OBBDownloaderService;
+import com.YourCompany.MasteringVR.DownloaderActivity;
+import android.app.Activity;
+import com.google.android.vending.expansion.downloader.Helpers;
+import com.YourCompany.MasteringVR.OBBData;
+
+
+public class DownloadShim
+{
+	public static OBBDownloaderService DownloaderService;
+	public static DownloaderActivity DownloadActivity;
+	public static Class<DownloaderActivity> GetDownloaderType() { return DownloaderActivity.class; }
+	public static boolean expansionFilesDelivered(Activity activity) {
+		for (OBBData.XAPKFile xf : OBBData.xAPKS) {
+			String fileName = Helpers.getExpansionAPKFileName(activity, xf.mIsMain, xf.mFileVersion, OBBData.AppType);
+			GameActivity.Log.debug("Checking for file : " + fileName);
+			String fileForNewFile = Helpers.generateSaveFileName(activity, fileName);
+			String fileForDevFile = Helpers.generateSaveFileNameDevelopment(activity, fileName);
+			GameActivity.Log.debug("which is really being resolved to : " + fileForNewFile + "\n Or : " + fileForDevFile);
+			if (Helpers.doesFileExist(activity, fileName, xf.mFileSize, false)) {
+				GameActivity.Log.debug("Found OBB here: " + fileForNewFile);
+			}
+			else if (Helpers.doesFileExistDev(activity, fileName, xf.mFileSize, false)) {
+				GameActivity.Log.debug("Found OBB here: " + fileForDevFile);
+			}
+			else return false;
+		}
+		return true;
+	}
+}
+
